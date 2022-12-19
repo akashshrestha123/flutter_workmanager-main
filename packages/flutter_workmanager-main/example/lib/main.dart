e4515b4128607a8b3334d583748ed5e9680e22aa
Akash Shrestha <akash@vurilo.com>
2022-12-19 15:38:46 +0545

    update

---
+++ packages/flutter_workmanager-main/example/lib/main.dart
@@ -0,0 +1,161 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:workmanager/workmanager.dart';
+
+void main() => runApp(MyApp());
+
+const simpleTaskKey = "be.tramckrijte.workmanagerExample.simpleTask";
+const rescheduledTaskKey = "be.tramckrijte.workmanagerExample.rescheduledTask";
+const failedTaskKey = "be.tramckrijte.workmanagerExample.failedTask";
+const simpleDelayedTask = "be.tramckrijte.workmanagerExample.simpleDelayedTask";
+const simplePeriodicTask =
+    "be.tramckrijte.workmanagerExample.simplePeriodicTask";
+const simplePeriodic1HourTask =
+    "be.tramckrijte.workmanagerExample.simplePeriodic1HourTask";
+
+@pragma(
+    'vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
+void callbackDispatcher() {
+  print("fafjaflajflajf");
+  Timer.periodic(const Duration(seconds: 1), (timer) async {
+    print("the time is");
+  });
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter WorkManager Example"),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  "Plugin initialization",
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                ElevatedButton(
+                  child: Text("Start the Flutter background service"),
+                  onPressed: () {
+                    Workmanager().initialize(
+                      callbackDispatcher,
+                      isInDebugMode: true,
+                    );
+                  },
+                ),
+                SizedBox(height: 16),
+
+                //This task runs once.
+                //Most likely this will trigger immediately
+                ElevatedButton(
+                  child: Text("Register OneOff Task"),
+                  onPressed: () {
+                    Workmanager().registerOneOffTask(
+                      simpleTaskKey,
+                      simpleTaskKey,
+                      inputData: <String, dynamic>{
+                        'int': 1,
+                        'bool': true,
+                        'double': 1.0,
+                        'string': 'string',
+                        'array': [1, 2, 3],
+                      },
+                    );
+                  },
+                ),
+                ElevatedButton(
+                  child: Text("Register rescheduled Task"),
+                  onPressed: () {
+                    Workmanager().registerOneOffTask(
+                      rescheduledTaskKey,
+                      rescheduledTaskKey,
+                      inputData: <String, dynamic>{
+                        'key': Random().nextInt(64000),
+                      },
+                    );
+                  },
+                ),
+                ElevatedButton(
+                  child: Text("Register failed Task"),
+                  onPressed: () {
+                    Workmanager().registerOneOffTask(
+                      failedTaskKey,
+                      failedTaskKey,
+                    );
+                  },
+                ),
+                //This task runs once
+                //This wait at least 10 seconds before running
+                ElevatedButton(
+                    child: Text("Register Delayed OneOff Task"),
+                    onPressed: () {
+                      Workmanager().registerOneOffTask(
+                        simpleDelayedTask,
+                        simpleDelayedTask,
+                        initialDelay: Duration(seconds: 10),
+                      );
+                    }),
+                SizedBox(height: 8),
+                //This task runs periodically
+                //It will wait at least 10 seconds before its first launch
+                //Since we have not provided a frequency it will be the default 15 minutes
+                ElevatedButton(
+                    child: Text("Register Periodic Task (Android)"),
+                    onPressed: Platform.isAndroid
+                        ? () {
+                            Workmanager().registerPeriodicTask(
+                              simplePeriodicTask,
+                              simplePeriodicTask,
+                              initialDelay: Duration(seconds: 10),
+                            );
+                          }
+                        : null),
+                //This task runs periodically
+                //It will run about every hour
+                ElevatedButton(
+                    child: Text("Register 1 hour Periodic Task (Android)"),
+                    onPressed: Platform.isAndroid
+                        ? () {
+                            Workmanager().registerPeriodicTask(
+                              simplePeriodicTask,
+                              simplePeriodic1HourTask,
+                              frequency: Duration(hours: 1),
+                            );
+                          }
+                        : null),
+                SizedBox(height: 16),
+                Text(
+                  "Task cancellation",
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                ElevatedButton(
+                  child: Text("Cancel All"),
+                  onPressed: () async {
+                    await Workmanager().cancelAll();
+                    print('Cancel all tasks completed');
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
